64610e258ae33ee744919776c0fcbf2919829789
chiperiandrei <andrei.chiperi98@gmail.com>
2019-05-10 12:48:34 +0300

    controller solved must to set Controller path in  Application file

--- controllers/ContactController.php
+++ controllers/ContactController.php
@@ -16,4 +16,22 @@ class ContactController extends Controller
 
         $this->view->render('contact/index');
     }
+    public function contact(){
+        $pentru = 'potrtw@gmail.com';
+        $firstname=$_POST["first-name"];
+        $lastname=$_POST["last-name"];
+        $email=$_POST["email"];
+        $text=$_POST["text"];
+        $subject = 'Contact form @ WEBSITE';
+        $mesaj = "Hello! " . $firstname . " ".$lastname." told you : \n
+            \" ".$text."\"
+                ";
+        $headers = 'From: webmaster@example.com' . "\r\n" .
+            'Reply-To: '.$email. "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+
+        mail($pentru, $subject, $mesaj, $headers);
+
+
+    }
 }
--- views/contact/index.php
+++ views/contact/index.php
@@ -13,30 +13,32 @@ if (Session::exists('user_id')) {
 ?>
 
 <main>
+    <form action="/contact/contact" method="post">
     <div class="container">
         <div class="first-name">
             <label for="first_name">First name:</label>
-            <input type="text" placeholder="John" id="first_name" required>
+            <input type="text" name="first-name" placeholder="John" id="first_name" required>
             <span><i class="fas fa-user"></i></span>
         </div>
         <div class="last-name">
             <label for="last_name">Last name:</label>
-            <input type="text" placeholder="Doe" id="last_name" required>
+            <input type="text" name="last-name" placeholder="Doe" id="last_name" required>
             <span><i class="fas fa-feather-alt"></i></span>
         </div>
         <div class="email">
             <label for="email">Email:</label>
-            <input type="text" placeholder="john.doe@example.com" id="email" required>
+            <input type="text" name="email" placeholder="john.doe@example.com" id="email" required>
             <span><i class="fas fa-at"></i></span>
         </div>
         <div class="textarea">
             <label for="textarea">Content for your message:</label>
-            <textarea></textarea>
+            <textarea name="text"></textarea>
         </div>
         <div class="submit">
             <button>Send<i class="fas fa-reply"></i></button>
         </div>
     </div>
+    </form>
 </main>
 
 <script src="public/js/main.js" type="text/javascript"></script>
