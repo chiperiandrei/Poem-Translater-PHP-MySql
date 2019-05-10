416fd4949cc8ad1bf4ae11254501feb594c503d7
Deny Patrascu <denypatrascu@gmail.com>
2019-05-10 20:37:39 +0300

    Update: Fixing Contact Layout

--- views/contact/index.php
+++ views/contact/index.php
@@ -1,6 +1,6 @@
 <?php require_once('views/components/meta.php'); ?>
 
-<link rel="stylesheet" href="public/css/contact.css">
+<link rel="stylesheet" href="/public/css/contact.css">
 
 <?php
 
@@ -13,34 +13,34 @@ if (Session::exists('user_id')) {
 ?>
 
 <main>
-    <form action="/contact/contact" method="post">
     <div class="container">
-        <div class="first-name">
-            <label for="first_name">First name:</label>
-            <input type="text" name="first-name" placeholder="John" id="first_name" required>
-            <span><i class="fas fa-user"></i></span>
-        </div>
-        <div class="last-name">
-            <label for="last_name">Last name:</label>
-            <input type="text" name="last-name" placeholder="Doe" id="last_name" required>
-            <span><i class="fas fa-feather-alt"></i></span>
-        </div>
-        <div class="email">
-            <label for="email">Email:</label>
-            <input type="text" name="email" placeholder="john.doe@example.com" id="email" required>
-            <span><i class="fas fa-at"></i></span>
-        </div>
-        <div class="textarea">
-            <label for="textarea">Content for your message:</label>
-            <textarea name="text"></textarea>
-        </div>
-        <div class="submit">
-            <button>Send<i class="fas fa-reply"></i></button>
-        </div>
+        <form action="/contact/contact" method="post">
+            <div class="first-name">
+                <label for="first_name">First name:</label>
+                <input type="text" name="first-name" placeholder="John" id="first_name" required>
+                <span><i class="fas fa-user"></i></span>
+            </div>
+            <div class="last-name">
+                <label for="last_name">Last name:</label>
+                <input type="text" name="last-name" placeholder="Doe" id="last_name" required>
+                <span><i class="fas fa-feather-alt"></i></span>
+            </div>
+            <div class="email">
+                <label for="email">Email:</label>
+                <input type="text" name="email" placeholder="john.doe@example.com" id="email" required>
+                <span><i class="fas fa-at"></i></span>
+            </div>
+            <div class="textarea">
+                <label for="textarea">Content for your message:</label>
+                <textarea name="text"></textarea>
+            </div>
+            <div class="submit">
+                <button>Send<i class="fas fa-reply"></i></button>
+            </div>
+        </form>
     </div>
-    </form>
 </main>
 
-<script src="public/js/main.js" type="text/javascript"></script>
+<script src="/public/js/main.js" type="text/javascript"></script>
 
 <?php require_once('views/components/footer.php'); ?>
