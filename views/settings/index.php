b58dc93a3556242d5b306e26947b14142b01d757
chiperiandrei <andrei.chiperi98@gmail.com>
2019-05-11 15:31:52 +0300

    errors for settings page

--- views/settings/index.php
+++ views/settings/index.php
@@ -17,60 +17,62 @@ if (Session::exists('user_id')) {
         <br>
         <br>
         <br>
-        <br>        <br>
-        <br>        <br>
-        <br>        <br>
-        <br>        <br>
-        <br>        <br>
+        <br> <br>
+        <br> <br>
+        <br> <br>
+        <br> <br>
+        <br> <br>
         <br>
 
 
-
-
-
-
-            <div class="container">
-                UPDATE INFO !!!!!!!
-                <div class="first-name">
-                    <label for="first-name">First name:</label>
-                    <input type="text" name="first-name" placeholder="John" required>
-                    <span><i class="fas fa-user"></i></span>
-                </div>
-                <div class="last-name">
-                    <label for="last-name">Last name:</label>
-                    <input type="text" name="last-name" placeholder="Doe" required>
-                </div>
-                <div class="email">
-                    <label for="email">Email:</label>
-                    <input type="email" name="email" placeholder="johndoe@exemple.com" required>
-                    <span><i class="fas fa-at"></i></span>
-                </div>
-                <div class="username">
-                    <label for="username">Username:</label>
-                    <input type="text" name="username" placeholder="john_doe" required>
-                </div>
-                <div class="password">
-                    <label for="password">Password:</label>
-                    <input type="password" name="password" placeholder="*******" required>
-                    <span><i class="fas fa-key"></i></span>
-                </div>
-                <div class="repeat-password">
-                    <label for="repeat-password">Repeat password:</label>
-                    <input type="password" name="repeat-password" placeholder="*******" required>
-                </div>
-                <div class="submit">
-                    <button type="submit"><i class="fas fa-share"></i>Send</button>
-                </div>
+        <div class="container">
+            UPDATE INFO !!!!!!!
+            <div class="first-name">
+                <label for="first-name">First name:</label>
+                <input type="text" name="first-name" placeholder="John" required>
+                <span><i class="fas fa-user"></i></span>
             </div>
+            <div class="last-name">
+                <label for="last-name">Last name:</label>
+                <input type="text" name="last-name" placeholder="Doe" required>
+            </div>
+            <div class="email">
+                <label for="email">Email:</label>
+                <input type="email" name="email" placeholder="johndoe@exemple.com" required>
+                <span><i class="fas fa-at"></i></span>
+            </div>
+            <div class="username">
+                <label for="username">Username:</label>
+                <input type="text" name="username" placeholder="john_doe" required>
+            </div>
+            <div class="password">
+                <label for="password">Password:</label>
+                <input type="password" name="password" placeholder="*******" required>
+                <span><i class="fas fa-key"></i></span>
+            </div>
+            <div class="repeat-password">
+                <label for="repeat-password">Repeat password:</label>
+                <input type="password" name="repeat-password" placeholder="*******" required>
+            </div>
+            <div class="submit">
+                <button type="submit"><i class="fas fa-share"></i>Send</button>
+            </div>
+        </div>
     </form>
 
-<font color="red">UPDATE PHOTO!!!!!!!!!</font>
+    <font color="red">UPDATE PHOTO!!!!!!!!!</font>
     <form action="/settings/edit-photo" method="post" enctype="multipart/form-data">
         Select image to upload:
         <input type="file" name="fileToUpload" id="fileToUpload">
         <input type="submit" value="Upload Image" name="submit">
     </form>
-<?php endif;?>
+<?php else:
+    http_response_code(403);
+    require_once('views/errors/404.php');
+    exit();
+
+
+endif; ?>
 <?php if (Session::exists('update-info-complete')) : ?>
     <div>
                         <span id="login-ok">
@@ -108,7 +110,6 @@ if (Session::exists('user_id')) {
                         </span>
     </div>
 <?php endif; ?>
-
 
 
 <?php if (Session::exists('upload-complete')) : ?>
