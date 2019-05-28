36b87472781b0700c1f9f091a97612df855ac4ec
Deny Patrascu <denypatrascu@gmail.com>
2019-05-28 11:20:23 +0300

    Update: fix error header

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,9 @@ toggleHeaderMenu = () => {
     const userMenuElement = document.getElementById('user-menu');
     userMenuElement.classList.toggle('hidden');
 }
-
+/*
 window.onload = function() {
     const navigationToggleElement = document.getElementById('navigation-user');
     navigationToggleElement.addEventListener('click', toggleHeaderMenu);
 }
+*/
