a9877e8f00cde041c26457d038a40be7717a365a
chiperiandrei <andrei.chiperi98@gmail.com>
2019-05-28 21:31:14 +0300

    added some files for rss subdomain

---
+++ rss/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RSS Feed - Powered by PoTr Team</title>
+</head>
+<body>
+<a href="news.rss" style="
+    font-size: 480px;
+    position: relative;
+    text-decoration: none;
+    padding-left: 7%;
+    color: black;
+">All news</a>
+</body>
+</html>
