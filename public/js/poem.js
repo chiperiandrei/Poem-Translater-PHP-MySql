665bfc0bbb3ab7f29de0a44afe9b522cff1ebb6a
Deny Patrascu <denypatrascu@gmail.com>
2019-05-29 22:53:48 +0300

    Update: XMLHttpRequest for comments

--- public/js/poem.js
+++ public/js/poem.js
@@ -40,3 +40,77 @@ hideAddTranslation = () => {
         translation.style.display = 'none';
     }, 200);
 };
+
+addComment = () => {
+    let XMLHttp;
+    const URL = window.location.href + '/add-comment';
+    const textareaValue = document.getElementById('add-comment').value;
+    const params = 'add-comment=' + textareaValue;
+
+    if (window.XMLHttpRequest) {
+        XMLHttp = new XMLHttpRequest();
+    } else {
+        XMLHttp = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+
+    XMLHttp.open('POST', URL,true);
+    XMLHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    XMLHttp.onreadystatechange = () => {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(XMLHttp.responseText);
+        }
+    };
+
+    let commentArea  = document.getElementById('comment-area');
+    let noCommentArea = document.getElementById('no-comment-area');
+
+    if (noCommentArea) {
+        commentArea.removeChild(noCommentArea);
+    }
+
+    // setting avatar
+    let avatar = document.getElementById('js-avatar');
+    const avatarImg = avatar.innerHTML.trim();
+    avatar = document.createElement('div');
+    avatar.setAttribute('class', 'avatar');
+    avatar.innerHTML = avatarImg;
+
+    // setting name
+    let name = document.getElementById('js-name');
+    const nameData = name.innerHTML.trim();
+    name = document.createElement('div');
+    name.setAttribute('class', 'name');
+    name.innerHTML = nameData;
+
+    // setting username
+    let username = document.getElementById('js-username');
+    const usernameData = username.innerHTML.trim();
+    username = document.createElement('div');
+    username.setAttribute('class', 'username');
+    username.innerHTML = usernameData;
+
+    // setting text
+    let text = document.createElement('div');
+    text.setAttribute('class', 'text');
+    text.innerHTML = textareaValue;
+
+    // setting final comment
+    let comment = document.createElement('div');
+    comment.setAttribute('class', 'comment');
+    comment.appendChild(avatar);
+    comment.appendChild(name);
+    comment.appendChild(username);
+    comment.appendChild(text);
+
+    // inserting the comment
+    commentArea.insertBefore(comment, commentArea.childNodes[0]);
+
+    if (XMLHttp.readyState === XMLHttp.DONE) {
+        if (XMLHttp.status === 200) {
+            console.log(XMLHttp.responseText);
+        }
+    }
+
+    XMLHttp.send(params);
+};
--- views/poem/components/comments.php
+++ views/poem/components/comments.php
@@ -1,5 +1,5 @@
 <div class="comments">
-    <section>
+    <section id="comment-area">
         <?php if ($this->poem_comments) : ?>
             <?php foreach ($this->poem_comments as $comment) : ?>
                 <div class="comment">
@@ -31,36 +31,63 @@
                 </div>
             <?php endforeach; ?>
         <?php else: ?>
-            <div class="no-comment">
+            <div id="no-comment-area" class="no-comment">
                 There is no comment yet.
             </div>
         <?php endif; ?>
     </section>
     <section>
         <?php if (Session::exists('user_id')) : ?>
-            <form action="<?php echo $this->poem_header['link']; ?>/add-comment" method="post">
-                <div class="add-comment">
-                    <div class="avatar">
-                        <img src="<?php Session::print('avatar'); ?>" alt="<?php Session::print('complete_name'); ?>">
-                    </div>
-                    <div class="name">
-                        <a href="<?php Session::print('user_link'); ?>">
-                            <?php Session::print('complete_name'); ?>
-                        </a>
-                    </div>
-                    <div class="username">
-                        <a href="<?php Session::print('user_link'); ?>">
-                            (<?php Session::print('username'); ?>)
-                        </a>
-                    </div>
-                    <div class="input">
-                        <textarea name="add-comment" id="add-comment"></textarea>
-                    </div>
-                    <div class="submit">
-                        <input type="submit" value="Add">
-                    </div>
+            <div class="add-comment">
+                <div class="avatar" id="js-avatar">
+                    <img src="<?php Session::print('avatar'); ?>" alt="<?php Session::print('complete_name'); ?>">
                 </div>
-            </form>
+                <div class="name" id="js-name">
+                    <a href="<?php Session::print('user_link'); ?>">
+                        <?php Session::print('complete_name'); ?>
+                    </a>
+                </div>
+                <div class="username" id="js-username">
+                    <a href="<?php Session::print('user_link'); ?>">
+                        (<?php Session::print('username'); ?>)
+                    </a>
+                </div>
+                <div class="input">
+                    <textarea name="add-comment" id="add-comment"></textarea>
+                </div>
+                <div class="submit">
+                    <button onclick="addComment()">Add</button>
+                </div>
+            </div>
         <?php endif; ?>
     </section>
 </div>
+
+<?php
+/*
+<?php if (Session::exists('user_id')) : ?>
+    <form action="<?php echo $this->poem_header['link']; ?>/add-comment" method="post">
+        <div class="add-comment">
+            <div class="avatar">
+                <img src="<?php Session::print('avatar'); ?>" alt="<?php Session::print('complete_name'); ?>">
+            </div>
+            <div class="name">
+                <a href="<?php Session::print('user_link'); ?>">
+                    <?php Session::print('complete_name'); ?>
+                </a>
+            </div>
+            <div class="username">
+                <a href="<?php Session::print('user_link'); ?>">
+                    (<?php Session::print('username'); ?>)
+                </a>
+            </div>
+            <div class="input">
+                <textarea name="add-comment" id="add-comment"></textarea>
+            </div>
+            <div class="submit">
+                <input type="submit" value="Add">
+            </div>
+        </div>
+    </form>
+<?php endif; ?>
+  */
